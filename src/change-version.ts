76c1fb4d929a57666c3a31ff731a9fe63f7ee254
Jonathan Schneider <jkschneider@gmail.com>
2025-04-16 10:20:30 -0400

    Basic recipe

--- src/change-version.ts
+++ src/change-version.ts
@@ -1,5 +1,5 @@
 import {ExecutionContext, Option, Recipe, Registered} from "@openrewrite/rewrite";
-import {Json, JsonVisitor, Member} from "@openrewrite/rewrite/json";
+import {Json, JsonKind, JsonVisitor, Member} from "@openrewrite/rewrite/json";
 
 @Registered
 export class ChangeVersion extends Recipe {
@@ -18,9 +18,18 @@ export class ChangeVersion extends Recipe {
     }
 
     get editor(): JsonVisitor<ExecutionContext> {
+        const v = this.version;
         return new class extends JsonVisitor<ExecutionContext> {
-            protected visitMember(member: Member, p: ExecutionContext): Promise<Json | undefined> {
-                return super.visitMember(member, p);
+            protected async visitMember(member: Member, p: ExecutionContext): Promise<Json | undefined> {
+                return this.produceJson<Member>(await super.visitMember(member, p), p, draft => {
+                    let key = member.key.element;
+                    if (key.kind === JsonKind.Literal && key.value === "version") {
+                        if (draft.value.kind === JsonKind.Literal) {
+                            draft.value.value = v;
+                            draft.value.source = `"${v}"`;
+                        }
+                    }
+                });
             }
         };
     }
