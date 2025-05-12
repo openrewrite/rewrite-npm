973ee62137e2683079e96637d215bae454076462
Jonathan Schneider <jkschneider@gmail.com>
2025-05-11 21:40:20 -0400

    Update with latest @openrewrite/rewrite

--- src/change-version.ts
+++ src/change-version.ts
@@ -1,5 +1,5 @@
 import {ExecutionContext, Option, Recipe} from "@openrewrite/rewrite";
-import {Json, JsonDocument, JsonKind, JsonVisitor, Member} from "@openrewrite/rewrite/json";
+import {Json, JsonVisitor} from "@openrewrite/rewrite/json";
 
 export class ChangeVersion extends Recipe {
     name = "org.openrewrite.npm.change-version";
@@ -19,7 +19,7 @@ export class ChangeVersion extends Recipe {
     get editor(): JsonVisitor<ExecutionContext> {
         const v = this.version;
         return new class extends JsonVisitor<ExecutionContext> {
-            protected async visitDocument(document: JsonDocument, p: ExecutionContext): Promise<Json | undefined> {
+            protected async visitDocument(document: Json.Document, p: ExecutionContext): Promise<Json | undefined> {
                 // Only visit package.json and package-lock.json files
                 if (!(document.sourcePath.endsWith("package.json") || document.sourcePath.endsWith("package-lock.json"))) {
                     return document;
@@ -27,11 +27,11 @@ export class ChangeVersion extends Recipe {
                 return super.visitDocument(document, p);
             }
 
-            protected async visitMember(member: Member, p: ExecutionContext): Promise<Json | undefined> {
-                return this.produceJson<Member>(await super.visitMember(member, p), p, draft => {
+            protected async visitMember(member: Json.Member, p: ExecutionContext): Promise<Json | undefined> {
+                return this.produceJson<Json.Member>(await super.visitMember(member, p), p, draft => {
                     let key = member.key.element;
-                    if (key.kind === JsonKind.Literal && key.value === "version") {
-                        if (draft.value.kind === JsonKind.Literal) {
+                    if (key.kind === Json.Kind.Literal && key.value === "version") {
+                        if (draft.value.kind === Json.Kind.Literal) {
                             draft.value.value = v;
                             draft.value.source = `"${v}"`;
                         }
